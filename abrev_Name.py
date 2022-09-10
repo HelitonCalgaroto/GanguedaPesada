85c1cebea93f3b36c18e930ea975df0d4ad3fa49
HelitonCalgaroto <whelyton_calgarotto@hotmail.com>
2022-09-09 22:40:40 -0300

    lol

--- abrev_Name.py
+++
@@ -1,8 +0,0 @@
-# Entrada: Alberto Moura da Silva Pereira
-def abrev_Name(fullname: []) -> str:
-    finalname = len(fullname[0]) + len(fullname[-1])
-
-    while finalname > 25:
-        for i in range(1, len(fullname) - 1):
-            if i > 2:
-                if finalname + len(i) >= 25:
